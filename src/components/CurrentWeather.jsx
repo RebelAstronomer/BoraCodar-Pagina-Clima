673fe188f9835f3936e0d8a47ad4e1120b4f127e
Arnaldo H. Saito <contato.arnaldohenrique@gmail.com>
2023-10-01 21:12:27 -0300

    add statistcs area in the project

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -1,4 +1,7 @@
 import './currentWeather.css';
+import Windy from '../assets/windy.png'
+import Humidity from '../assets/humidity.png'
+import MaybeRain from '../assets/maybe-rain.png'
 
 export default function CurrentWeather() {
   return (
@@ -13,12 +16,37 @@ export default function CurrentWeather() {
         <div className='degreesNumber'>
           <p>18</p>
         </div>
+        <div className='degrees'>
+          °C
+        </div>
         <div className='minMaxDegreesNumber'>
             <span className='minDegreesNumber'>22°</span>
             <span className='maxDegreesNumber'>16°</span>
         </div>
       </div>
-      <div className="statistcs"></div>
+      <div className="statistcs">
+        <div className='boxStatistcs'>
+          <img src={Windy} alt="Vento" />
+          <p className='infoStatistcs'>
+            Vento
+            <p>17 <span className='text-sm'>km/h</span></p>
+          </p>
+        </div>
+        <div className='boxStatistcs'>
+          <img src={Humidity} alt="Umidade" />
+            <p className='infoStatistcs'>
+              Umidade
+              <p>31 <span className='text-sm'>%</span></p>
+            </p>
+        </div>
+        <div className='boxStatistcs'>
+        <img src={MaybeRain} alt="Chuva" />
+          <p className='infoStatistcs'>
+            Chuva
+            <p>10 <span className='text-sm'>%</span></p>
+          </p>
+        </div>
+      </div>
       <div className="weatherIndicated"></div>
     </section>
   );
